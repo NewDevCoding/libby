6e325365aaf116bf8397724d1933c05e3fbb97ab
Isaac <newdevcoding@gmail.com>
2025-01-26 20:51:24 -0500

    main setup

---
+++ components/BookOverview.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BookOverview = () => {
+  return (
+    <section className='book-overview'>
+        <div className='flex flex-col gap-5 flex-1'>
+            <h1>An overview</h1>
+        </div>
+    </section>
+  )
+}
+
+export default BookOverview
