987a01fc5c167d240d9629b2ce1ccf6871c9cc85
Isaac <newdevcoding@gmail.com>
2025-02-02 14:41:50 -0500

    commit

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,5 +1,17 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/Input"
+
 import { zodResolver } from "@hookform/resolvers/zod"
 import { DefaultValues, useForm, UseFormReturn, SubmitHandler, FieldValues } from "react-hook-form"
 import { ZodType } from "zod";
@@ -31,7 +43,27 @@ const AuthForm = <T extends FieldValues> ({
     }
   
     return (
-        <div></div>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-7-8">
+                <FormField
+                    render={({ field}) => (
+                        <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                                <Input placeholder="shadcn" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                This is your public display name
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Submit</Button>
+
+            </form>
+        </Form>
+
     )
 }
 
